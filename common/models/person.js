8348ae969606199dbf11755bd0dd0e7ebe1050f2
malvrz <mary@perkd.me>
2023-03-03 17:33:18 +0800

    Added follow/unfollow endpoints
    Added followers/following list endpoints

--- common/models/person.js
+++ common/models/person.js
@@ -25,7 +25,7 @@ module.exports = function(Person) {
         const accessToken = req.query.access_token
 
         return AccessToken.findById(accessToken).then(async access => {
-            return Person.findById(access.userId)
+            return Person.findById(access.userId, {include: ['tweets']})
         })
     }
     Person.remoteMethod(
@@ -65,6 +65,139 @@ module.exports = function(Person) {
         }
     )
 
+    Person.follow = async function(req, id, next){
+        const accessToken = req.query.access_token
+        const { Followships } = Person.app.models
 
+        return AccessToken.findById(accessToken).then(async access => {
+            const filter = {
+                followedId: id,
+                followerId: access.userId
+            }
 
+            return await Followships.count(filter).then(c => {
+                if (c === 0 && filter.followerId !== filter.followedId) {
+                    return Followships.create(filter)
+                }
+            })
+        })
+    }
+    Person.remoteMethod(
+        'follow', {
+            http: {
+                path: '/:id/follow',
+                verb: 'post'
+            },
+            accepts: [
+                {arg: 'req', type: 'object', http: {source: 'req'}},
+                {arg: 'id', type: 'string', required: true}
+            ],
+            returns: {arg:'data', type: 'Followships', root: true}
+        }
+    )
+
+    Person.unfollow = async function(req, id){
+        const accessToken = req.query.access_token
+        const { Followships } = Person.app.models
+
+        return AccessToken.findById(accessToken).then(async access => {
+            const filter = {
+                followedId: id,
+                followerId: access.userId
+            }
+            const ret = await Followships.findOne(filter)
+            if (ret) return await Followships.deleteById(ret.id)
+        })
+    }
+    Person.remoteMethod(
+        'unfollow', {
+            http: {
+                path: '/:id/unfollow',
+                verb: 'post'
+            },
+            accepts: [
+                {arg: 'req', type: 'object', http: {source: 'req'}},
+                {arg: 'id', type: 'string', required: true}
+            ]
+        }
+    )
+
+    Person.getFollowers = async function(req, id){
+        const accessToken = req.query.access_token
+
+        return await AccessToken.findById(accessToken).then(async access => {
+            const rec = await getFollowshipsId({followedId: id}, 'followerId')
+            return Person.find(
+                {
+                    where: {id: {inq: [...rec]}}
+                }
+            )
+        })
+    }
+    Person.remoteMethod(
+        'getFollowers', {
+            http: {
+                path: '/:id/followers/list',
+                verb: 'get'
+            },
+            accepts: [
+                {arg: 'req', type: 'object', http: {source: 'req'}},
+                {arg: 'id', type: 'string', required: true}
+            ],
+            returns: {arg:'data', type: 'Person', root: true}
+        }
+    )
+
+    Person.getFollowing = async function(req, id) {
+        const accessToken = req.query.access_token
+
+        return await AccessToken.findById(accessToken).then(async access => {
+            const rec = await getFollowshipsId({followerId: id}, 'followedId')
+            return Person.find(
+                {
+                    where: {id: {inq: [...rec]}}
+                }
+            )
+        })
+    }
+    Person.remoteMethod(
+        'getFollowing', {
+            http: {
+                path: '/:id/following/list',
+                verb: 'get'
+            },
+            accepts: [
+                {arg: 'req', type: 'object', http: {source: 'req'}},
+                {arg: 'id', type: 'string', required: true}
+            ],
+            returns: {arg:'data', type: 'Person', root: true}
+        }
+    )
+
+    // ------ Private functions ----- //
+
+    async function getFollowshipsId(filter, key) {
+        const { Followships } = Person.app.models
+        let ret = []
+
+        return await Followships.find({where: filter}).then(async records => {
+            for (const record of records) {
+                ret.push(record[key])
+            }
+            return ret
+        })
+    }
+
+    // ------ End of private functions ----- //
+
+    Person.disableRemoteMethodByName('prototype.__create__followships')
+    Person.disableRemoteMethodByName('prototype.__get__followships')
+    Person.disableRemoteMethodByName('prototype.__delete__followships')
+    Person.disableRemoteMethodByName('prototype.__findById__followships')
+    Person.disableRemoteMethodByName('prototype.__updateById__followships')
+    Person.disableRemoteMethodByName('prototype.__destroyById__followships')
+    Person.disableRemoteMethodByName('prototype.__count__followships')
+    Person.disableRemoteMethodByName('prototype.__exists__followships')
+    Person.disableRemoteMethodByName('prototype.__link__followships')
+    Person.disableRemoteMethodByName('prototype.__unlink__followships')
 };
